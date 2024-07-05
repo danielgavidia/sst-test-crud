12690eb3966d548043074e729584ee8b2c4e464f
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-05 08:02:14 -0400

    added logic for getting a specific note

---
+++ packages/functions/list.ts
@@ -0,0 +1,23 @@
+import { DynamoDB } from "aws-sdk";
+import { Table } from "sst/node/table";
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
+export async function main() {
+    const params = {
+        // Get the table name from the environment variable
+        TableName: Table.Notes.tableName,
+        // Get all the rows where the userId is our hardcoded user id
+        KeyConditionExpression: "userId = :userId",
+        ExpressionAttributeValues: {
+            ":userId": "123",
+        },
+    };
+
+    const results = await dynamoDb.query(params).promise();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(results.Items),
+    };
+}
